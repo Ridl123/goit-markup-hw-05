d1b68cdaa3d2f7f999992465ff33252c9dda2320
Ridl123 <purdel_richard@yahoo.com>
2024-01-08 17:33:38 +0200

    modal errors

--- js/modal.js
+++ js/modal.js
@@ -12,22 +12,3 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
-// is-hidden {
-//   opacity: 0;
-//   visibility: hidden;
-//   pointer-events: none;
-// }
-// modal-window-button:hover,
-// modal-window-button:focus {
-//   background-color: #404bbf;
-//   border: none;
-// }
-// close-button {
-//   transition: fill 250ms cubic-bezier(0.4, 0, 0.2, 1);
-//   margin: 1px solid rgba(0, 0, 0, 0, 0.1);
-//   transition: background-colour 250ms cubic-bezier(0.4, 0, 0.2, 1),border 250ms cubic-bezier(0.4, 0, 0.2, 1);
-// }
-// close-button:hover,
-// close-button:focus {
-//   fill: #ffffff;
-// }
