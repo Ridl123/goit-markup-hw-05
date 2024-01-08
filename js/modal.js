7f651d0c0c75a5fc149ba19528b9ec23deae19ab
Ridl123 <purdel_richard@yahoo.com>
2024-01-08 16:06:33 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -17,3 +17,17 @@ is-hidden {
   visibility: hidden;
   pointer-events: none;
 }
+modal-window-button:hover,
+modal-window-button:focus {
+  background-color: #404bbf;
+  border: none;
+}
+close-button {
+  transition: fill 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  margin: 1px solid rgba(0, 0, 0, 0, 0.1);
+  transition: background-colour 250ms cubic-bezier(0.4, 0, 0.2, 1),border 250ms cubic-bezier(0.4, 0, 0.2, 1);
+}
+close-button:hover,
+close-button:focus {
+  fill: #ffffff;
+}
